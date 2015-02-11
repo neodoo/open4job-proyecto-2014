d4f3fd737e8989f196c0541070fe7177ca571817
lchacon <lchaconperez@gmail.com>
2015-02-11 10:00:18 +0100

    Subida del modelo del Alumno y las páginas de pruebas para probar la captura de parámetros y vista del detalle del alumno.

---
+++ src/main/java/es/opensigad/model/dao/AlumnoDAO.java
@@ -0,0 +1,131 @@
+package es.opensigad.model.dao;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import es.opensigad.model.vo.AlumnoVO;
+
+@ManagedBean
+@SessionScoped
+public class AlumnoDAO implements AlumnoDaoInterfaz, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final Logger logger = Logger.getLogger(AlumnoVO.class
+			.getName());
+
+	private DataSource ds;
+
+	private AlumnoVO alumnoVO;
+
+	public AlumnoVO getAlumnoVO() {
+		return alumnoVO;
+	}
+
+	public void setAlumnoVO(AlumnoVO alumnoVO) {
+		this.alumnoVO = alumnoVO;
+	}
+
+	public AlumnoDAO() {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/opensigad");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<AlumnoVO> getListAlumno() {
+
+		List<AlumnoVO> listAlumno = null;
+		Connection con = null;
+		AlumnoVO alumnoVO = null;
+		PreparedStatement ps = null;
+		ResultSet result = null;
+
+		try {
+			new ArrayList<AlumnoVO>();
+			con = ds.getConnection();
+
+			alumnoVO = null;
+			ps = con.prepareStatement("SELECT * FROM alumno");
+
+			result = ps.executeQuery();
+
+			while (result.next()) {
+				alumnoVO = new AlumnoVO(result.getInt("id"),
+						result.getInt("num_expediente"),
+						result.getString("nombre"),
+						result.getString("apellido1"),
+						result.getString("apellido2"),
+						result.getString("sexo"), result.getString("dni"),
+						result.getString("telefono"),
+						result.getDate("fecha_nacimiento"),
+						result.getString("pais"),
+						result.getString("provincia"),
+						result.getString("localidad"),
+						result.getString("domicilio"),
+						result.getString("email"));
+
+				listAlumno.add(alumnoVO);
+			}
+		} catch (SQLException e) {
+			listAlumno = null;
+			logger.log(Level.SEVERE, "SQLException : " + e.getMessage());
+		}
+
+		return listAlumno;
+	}
+
+	public AlumnoVO getDetalleAlumno(int num_expediente) {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet result = null;
+
+		try {
+			new ArrayList<AlumnoVO>();
+			con = ds.getConnection();
+
+			this.alumnoVO = null;
+			ps = con.prepareStatement("SELECT * FROM alumno where id =?");
+			ps.setInt(1, num_expediente);
+
+			result = ps.executeQuery();
+
+			while (result.next()) {
+				this.alumnoVO = new AlumnoVO(result.getInt("id"),
+						result.getInt("num_expediente"),
+						result.getString("nombre"),
+						result.getString("apellido1"),
+						result.getString("apellido2"),
+						result.getString("sexo"), result.getString("dni"),
+						result.getString("telefono"),
+						result.getDate("fecha_nacimiento"),
+						result.getString("pais"),
+						result.getString("provincia"),
+						result.getString("localidad"),
+						result.getString("domicilio"),
+						result.getString("email"));
+			}
+		} catch (SQLException e) {
+			logger.log(Level.SEVERE, "SQLException : " + e.getMessage());
+		}
+
+		return alumnoVO;
+	}
+}
---
+++ src/main/java/es/opensigad/model/vo/AlumnoVO.java
@@ -0,0 +1,169 @@
+package es.opensigad.model.vo;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class AlumnoVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private int num_expediente;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String sexo;
+	private String dni;
+	private String telefono;
+	private Date fecha_acimiento;
+	private String pais;
+	private String provincia;
+	private String localidad;
+	private String domicilio;
+	private String email;
+	
+	public AlumnoVO(){
+		super();
+	}
+
+	public AlumnoVO(int id, int num_expediente, String nombre,
+			String apellido1, String apellido2, String sexo, String dni,
+			String telefono, Date fecha_nacimiento, String pais,
+			String provincia, String localidad, String domicilio, String email) {
+
+		this.id = id;
+		this.num_expediente = num_expediente;
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+		this.sexo = sexo;
+		this.dni = dni;
+		this.telefono = telefono;
+		this.fecha_acimiento = fecha_nacimiento;
+		this.pais = pais;
+		this.provincia = provincia;
+		this.localidad = localidad;
+		this.domicilio = domicilio;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNum_expediente() {
+		return num_expediente;
+	}
+
+	public void setNum_expediente(int num_expediente) {
+		this.num_expediente = num_expediente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Date getFecha_acimiento() {
+		return fecha_acimiento;
+	}
+
+	public void setFecha_acimiento(Date fecha_acimiento) {
+		this.fecha_acimiento = fecha_acimiento;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String toString() {
+		return ("AlumnoDAO[ id = " + id + " num_expediente = " + num_expediente );
+	}
+}
