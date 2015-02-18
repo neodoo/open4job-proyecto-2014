0cc9ec2b4b5ebc2d5289fff96fac4c8eb2ef1605
adriancalanda <adriancalanda@gmail.com>
2015-02-18 10:38:24 +0100

    Reconstrucción de los DAO AlumnoMatriculaDAO y Interfaz. Y Creación del AlumnoMatriculaVO

--- src/main/java/es/opensigad/model/dao/AlumnoMatriculaDAOInterfaz.java
+++ src/main/java/es/opensigad/model/dao/AlumnoMatriculaDAOInterfaz.java
@@ -1,19 +1,19 @@
 package es.opensigad.model.dao;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 import es.opensigad.model.vo.AlumnoMatriculaVO;
 
 public interface AlumnoMatriculaDAOInterfaz {
 
-	public boolean insertarMatricula(int idAlumno, String fecha, String centro,
-			String tipoEnsenanza, String ensenanza, String curso);
+	public boolean insertarMatricula(int idAlumno,int cursoEscolar, String centro,
+			String ensenanza, String modulo, int curso);
 
 	public boolean borrarMatricula(int idMatricula);
 
-	public boolean modificarMatricula(int idAlumno, String fecha,
-			String centro, String tipoEnsenanza, String ensenanza, String curso, int idMatricula);
+	public boolean modificarMatricula(int idAlumno, int cursoEscolar,
+			String centro, String ensenanza, String modulo,
+			int curso, int idMatricula);
 
 	public ArrayList<AlumnoMatriculaVO> getListadoMatricula(int idAlumno);
 	
