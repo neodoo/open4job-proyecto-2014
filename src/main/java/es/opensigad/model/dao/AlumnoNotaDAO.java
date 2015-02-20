ca9efdc121d1cb722bc576c6560faae97e6441b0
alariel91 <monysaenzcarrero@gmail.com>
2015-02-20 13:11:22 +0100

    #2 Agregar getAllAlumnoNotas - Metodo que te devuelve el ArrayList del listado de notas

--- src/main/java/es/opensigad/model/dao/AlumnoNotaDAO.java
+++ src/main/java/es/opensigad/model/dao/AlumnoNotaDAO.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.sql.DataSource;
 
 import es.opensigad.model.vo.AlumnoNota;
@@ -24,7 +27,16 @@ public class AlumnoNotaDAO {
 	}
 
 	public ArrayList<AlumnoNota> getAllAlumnoNotas() {
-		return null;
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("persistenceUnit");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		ArrayList<AlumnoNota> listaNotas = (ArrayList<AlumnoNota>) em
+				.createQuery("from AlumnoNota").getResultList();
+		em.getTransaction().commit();
+
+		em.close();
+		return listaNotas;
 		
 	}
 
