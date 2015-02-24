54cce10d463cba718c436ea2e279937f3ea55d7f
fsolans <francisco.solans@neodoo.es>
2015-02-24 10:11:09 +0100

    Refactorización del código para mantenerlo de la misma forma que el resto. Para ello, se han modificado los DAO, Bean y las clases de tests.

--- src/test/java/es/opensigad/model/dao/AlumnoNotaDAOTest.java
+++ src/test/java/es/opensigad/model/dao/AlumnoNotaDAOTest.java
@@ -1,33 +1,43 @@
 package es.opensigad.model.dao;
 
+import java.util.List;
+
 import junit.framework.TestCase;
-
-import java.util.ArrayList;
-
-import es.opensigad.controller.VerAlumnoNotaListadoBean;
 import es.opensigad.model.vo.AlumnoNota;
-
 
 public class AlumnoNotaDAOTest extends TestCase {
 
 	public void testGetNotasByIdMatricula() {
-		AlumnoNotaDaoPoolDB alumnoDao = new  AlumnoNotaDaoPoolDB();
-		ArrayList <AlumnoNota> alumnoNotas = alumnoDao.getNotasByIdMatricula(1);
-		assertTrue(alumnoNotas.size()>0);
-	}
-/*
-	public void testInsertarBorrarNotasAlumnoByIdMatricula() {
+		
 		AlumnoNotaDaoPoolDB alumnoDao = new AlumnoNotaDaoPoolDB();
-		boolean estado = alumnoDao.insertarNotasAlumnoByIdMatricula(99, 99, 99, 3, 9);
-		assertTrue(estado);
-		estado = alumnoDao.borrarNotaByIdMatricula(99);
-		assertTrue(estado);
+		List<AlumnoNota> alumnoNotas = alumnoDao.getNotasByIdMatricula(1);
+	
+		assertTrue(alumnoNotas.size() > 0);
+	
 	}
-*/
+
+	/*
+	 public void testInsertarBorrarNotasAlumnoByIdMatricula() {
+	 
+	 	AlumnoNotaDaoPoolDB alumnoDao = new AlumnoNotaDaoPoolDB(); 
+	 	boolean estado = alumnoDao.insertarNotasAlumnoByIdMatricula(99, 99, 99, 3, 9);
+	 
+	 	assertTrue(estado); 
+	 	estado = alumnoDao.borrarNotaByIdMatricula(99);
+	 	
+	 	assertTrue(estado); 
+	 	
+	 }
+	 */
+	
 	public void testActualizarNotaByIdMatricula() {
+		
 		AlumnoNotaDaoPoolDB alumnoDao = new AlumnoNotaDaoPoolDB();
-		//boolean estado = alumnoDao.actualizarNotaByIdMatricula(99, 99, 99, 99,"2","2");
-		//assertTrue(estado);
+		// boolean estado = alumnoDao.actualizarNotaByIdMatricula(99, 99, 99,
+		// 99,"2","2");
+	
+		// assertTrue(estado);
+	
 	}
 
 }
