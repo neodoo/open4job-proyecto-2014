e4b6c78081b40b8a5c2e243cb6f767ce7562a0aa
lgantonio <lgantonio@hotmail.com>
2015-02-25 11:14:11 +0100

    #24 Modificaciones test

--- src/test/java/es/opensigad/model/dao/AlumnoSeguimientoDAOTest.java
+++ src/test/java/es/opensigad/model/dao/AlumnoSeguimientoDAOTest.java
@@ -3,30 +3,37 @@ package es.opensigad.model.dao;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import es.opensigad.model.vo.AlumnoMatricula;
 import es.opensigad.model.vo.AlumnoSeguimiento;
+import es.opensigad.model.vo.EnsenanzaMateria;
 import junit.framework.TestCase;
 
 public class AlumnoSeguimientoDAOTest extends TestCase {
 
 	public static int idSeguimiento = 0;
-	public static int idAlumnoMatricula = 999999999;
+	public static int idMatricula = 6;
+	public static int idEnsenanza = 1;
 
 	AlumnoSeguimientoDAOPoolDB alumnoSeguimientoDAO = new AlumnoSeguimientoDAOPoolDB();
 
 	java.util.Date utilDate = new java.util.Date();
 	java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-	
+
 	public void test1() {
+		// Alumno-Seguimiento
+		AlumnoSeguimiento alumnoSeguimiento = new AlumnoSeguimiento();
 
 		// Alumno-Matricula
 		AlumnoMatricula alumnoMatricula = new AlumnoMatricula();
-		alumnoMatricula.setId(idAlumnoMatricula);
-		
-		// Alumno-Seguimiento
-		AlumnoSeguimiento alumnoSeguimiento = new AlumnoSeguimiento();
+		alumnoMatricula.setId(idMatricula);
 		alumnoSeguimiento.setAlumnoMatricula(alumnoMatricula);
+		
+		// Enseñanza-Materia
+		EnsenanzaMateria ensenanzaMateria = new EnsenanzaMateria();		
+		ensenanzaMateria.setId(idEnsenanza);
+		alumnoSeguimiento.setEnsenanzaMateria(ensenanzaMateria);
 	
 		Date fechaSql = null;
 		try {
@@ -38,7 +45,8 @@ public class AlumnoSeguimientoDAOTest extends TestCase {
 		alumnoSeguimiento.setFecha(fechaSql);
 
 		alumnoSeguimiento.setSesion("sesion_I_TEST");
-		alumnoSeguimiento.setTipo("tipo_I_TEST");
+		alumnoSeguimiento.setTipo("incidencia");
+		
 		alumnoSeguimiento.setJustificante(0);
 		alumnoSeguimiento.setObservaciones("observaciones_I_TEST");		
 		
@@ -49,19 +57,13 @@ public class AlumnoSeguimientoDAOTest extends TestCase {
 
 	public void test2() {
 		// Visualizar una falta
-		//AlumnoSeguimiento alumnoSeguimiento = alumnoSeguimiento.getDetalleFalta(idFalta);
-
-		//assertTrue(alumnoSeguimiento != null);
-
-	}
-
-	public void test3() {
-		// Actualizar Alumno-Seguimiento		
 		AlumnoSeguimiento alumnoSeguimiento = new AlumnoSeguimiento();
-	
+		alumnoSeguimiento = alumnoSeguimientoDAO.getDetalleAlumnoSeguimiento(idSeguimiento);
+		
+		// Actualizar Alumno-Seguimiento			
 		Date fechaSql = null;
 		try {
-			fechaSql = new SimpleDateFormat("dd/MM/yyy").parse("02/02/1902");
+			fechaSql = new SimpleDateFormat("dd/MM/yyyy").parse("02/02/1902");
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}				
@@ -69,7 +71,7 @@ public class AlumnoSeguimientoDAOTest extends TestCase {
 		alumnoSeguimiento.setFecha(fechaSql);
 
 		alumnoSeguimiento.setSesion("sesion_U_TEST");
-		alumnoSeguimiento.setTipo("tipo_U_TEST");
+		alumnoSeguimiento.setTipo("falta");
 		alumnoSeguimiento.setObservaciones("observaciones_U_TEST");		
 		
 		boolean estado = alumnoSeguimientoDAO.actualizarAlumnoSeguimiento(alumnoSeguimiento);		
@@ -77,15 +79,14 @@ public class AlumnoSeguimientoDAOTest extends TestCase {
 		assertTrue(estado);
 	}
 
-	public void test4() {
-		// Visualizar faltas por alumno
-		//List<AlumnoSeguimiento> alumnoFaltasVO = alumnoSeguimientoDAO
-		//		.getListaFaltas(idAlumno);
-
-		//assertNotNull(alumnoFaltasVO);
+	public void test3() {
+		// Visualizar faltas por matricula
+		List<AlumnoSeguimiento> alumnoSeguimiento = alumnoSeguimientoDAO.getListaAlumnoSeguimiento(idMatricula);
+		
+		assertNotNull(alumnoSeguimiento);
 	}
 
-	public void test5() {		
+	public void test4() {		
 		
 		// Eliminar Seguimiento
 		AlumnoSeguimiento alumnoSeguimiento = new AlumnoSeguimiento();
