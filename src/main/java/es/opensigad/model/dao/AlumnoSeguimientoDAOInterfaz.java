89429a09f1079ef7985e70d18698fb7f2bc45b4f
Galvez85 <jmgalvez85@gmail.com>
2015-03-06 13:33:31 +0100

    Modificado AlumnoSguimiento para incorporar los componentes distribuidos.

--- src/main/java/es/opensigad/model/dao/AlumnoSeguimientoDAOInterfaz.java
+++ src/main/java/es/opensigad/model/dao/AlumnoSeguimientoDAOInterfaz.java
@@ -2,8 +2,11 @@ package es.opensigad.model.dao;
 
 import java.util.List;
 
+import javax.ejb.Local;
+
 import es.opensigad.model.vo.AlumnoSeguimiento;
 
+@Local
 public interface AlumnoSeguimientoDAOInterfaz {
 
 	public List<AlumnoSeguimiento> getListaAlumnoSeguimiento(int pidMatricula);
