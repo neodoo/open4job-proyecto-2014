71487a31e0735eccce78d9ddaee98f132a065976
lchacon <lchaconperez@gmail.com>
2015-03-13 09:58:36 +0100

    #71 Añadidas variables de idAlumno Y NumExpediente en sesión

--- src/main/java/es/opensigad/controller/SesionBean.java
+++ src/main/java/es/opensigad/controller/SesionBean.java
@@ -7,8 +7,12 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class SesionBean {
 
+	private int idAlumno;
+	private int NumExpediente;
 	private int idMatricula;
 
+	// GETTERS Y SETTERS
+	
 	public int getIdMatricula() {
 		return idMatricula;
 	}
@@ -17,4 +21,19 @@ public class SesionBean {
 		this.idMatricula = idMatricula;
 	}
 
+	public int getIdAlumno() {
+		return idAlumno;
+	}
+
+	public void setIdAlumno(int idAlumno) {
+		this.idAlumno = idAlumno;
+	}
+
+	public int getNumExpediente() {
+		return NumExpediente;
+	}
+
+	public void setNumExpediente(int numExpediente) {
+		NumExpediente = numExpediente;
+	}
 }
