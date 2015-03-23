ed9cd01b42334e450a55dab507c8d2de7b88b01c
alariel91 <monysaenzcarrero@gmail.com>
2015-03-23 11:22:05 +0100

    #91 Homogeneizar la ficha de notas (En equipo) - Añadido mensajes de información a la hora de eliminar una nota.

--- src/main/java/es/opensigad/controller/EliminarAlumnoNotaBean.java
+++ src/main/java/es/opensigad/controller/EliminarAlumnoNotaBean.java
@@ -3,9 +3,14 @@ package es.opensigad.controller;
 import java.io.Serializable;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.hibernate.Session;
 
 import es.opensigad.model.dao.AlumnoNotaDAO;
 import es.opensigad.model.dao.AlumnoNotaDAOInterfaz;
@@ -38,13 +43,37 @@ public class EliminarAlumnoNotaBean implements Serializable {
 		this.alumnoNotaDAO = alumnoNotaDAO;
 	}
 
+	@ManagedProperty(value = "#{sesionBean}")
+	private SesionBean sesionBean;
+
+	public SesionBean getSesionBean() {
+		return sesionBean;
+	}
+
+	public void setSesionBean(SesionBean sesionBean) {
+		this.sesionBean = sesionBean;
+	}
+
+	private FacesMessage facesMessage;
+
 	public String borrarNotaById(int id) {
 		String pagina = null;
 		if (alumnoNotaDAO.borrarNotaById(id)) {
 			pagina = "verAlumnoNotaListado";
+			facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"La nota del alumno con matricula "
+							+ sesionBean.getIdMatricula()
+							+ " ha sido eliminada.", null);
+
 		} else {
 			pagina = "verAlumnoNotaListado";
+			facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"La nota del alumno con matricula "
+							+ sesionBean.getIdMatricula()
+							+ " NO ha sido eliminada.", null);
+
 		}
+		FacesContext.getCurrentInstance().addMessage(null, facesMessage);
 		return pagina;
 	}
 
